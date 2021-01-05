5ab7ace740c8428796e02d93a8c8045753925130
rahmathidayat9 <yeageirstsatu@gmail.com>
2021-01-06 04:42:39 +0700

    Make It Better

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -10,9 +10,7 @@ class RegisterController extends Controller
 
     public function send(Request $request)
     {
-        Mail::send('isiemail', array('pesan' => 'Apakah Email nya Masuk ?? Aatau Tidak'),function($pesan){
-            $pesan->to('yeageristlima@gmail.com')->subject('Verifikasi Akun Anda');
-            $pesan->from(env('MAIL_USERNAME','fitrisf96@gmail.com'),'Anime Info List');
-        });
+        Mail::to('yeageristlima@gmail.com')
+        ->send('Hi Please Confirm Your Account','This Email From App Using');
     }
 }
