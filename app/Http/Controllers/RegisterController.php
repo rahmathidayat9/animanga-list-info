1ac62a4cf6a603dceb75cf3e8cd0ae0333dce778
rahmathidayat9 <yeageirstsatu@gmail.com>
2021-01-06 04:36:07 +0700

    Make It Better

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -7,21 +7,12 @@ use Illuminate\Http\Request;
 
 class RegisterController extends Controller
 {
-	public function index()
-	{
-		return view('email.form');
-	}
 
-    public function send(Request $request){
-        try{
-            Mail::send('Hai , Segera Akivasi Akun Anda Sekarang Juga', array('pesan' => $request->pesan) , function($pesan) use($request){
-                $pesan->to('yeageristlima@gmail.com')->subject('Verifikasi Akun Anda');
-                $pesan->from(env('MAIL_USERNAME','fitrisf96@gmail.com'),'Anime Info List');
-                // $pesan->attach('https://i.imgur.com/JVLQCHr.jpg');
-	            //dd("Sukses ! Email berhasil dikirim");
-            });
-        }catch (Exception $e){
-            return response (['status' => false,'errors' => $e->getMessage()]);
-        }
+    public function send(Request $request)
+    {
+        Mail::send('isiemail', array('pesan' => 'Apakah Email nya Masuk ?? Aatau Tidak'),function($pesan){
+            $pesan->to('yeageristlima@gmail.com')->subject('Verifikasi Akun Anda');
+            $pesan->from(env('MAIL_USERNAME','fitrisf96@gmail.com'),'Anime Info List');
+        });
     }
 }
